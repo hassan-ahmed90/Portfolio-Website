8e2565a8faf141b25b6cc432a4ecf6995d4cc394
Hassan Ahmed <hassanahmed40005@gmail.com>
2024-06-15 15:58:27 +0500

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Hassan Ahmed",
+  title: "Data Scientist & Content Creator",
+  email: "hassanahmedrajput@gmail.com",
+  gitHub: "hassan-ahmed90",
+  instagram: "hassanshani",
+  linkedIn: "Hassan Ahmed",
+  medium: "Hassan Ahmed",
+  twitter: "Hassan Ahmed",
+  youTube: "Hassan Ahmed",
 };
 
 const primaryColor = "#4E567E";
